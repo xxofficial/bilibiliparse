166676d159b2f0d5b8eebd3757e33adbc1272519
nya <383353828@qq.com>
2025-12-08 17:54:58 +0800

    test

--- main.py
+++ main.py
@@ -20,5 +20,10 @@ class MyPlugin(Star):
         logger.info(message_chain)
         yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
 
+    @filter.event_message_type(filter.EventMessageType.ALL)
+    async def on_all_message(self, event: AstrMessageEvent):
+        logger.info(event.raw_message)
+        yield event.plain_result("收到了一条消息。")
+
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
