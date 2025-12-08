c736f3981fce8217dd750b49aab52080fb18dd03
nya <383353828@qq.com>
2025-12-08 19:01:33 +0800

    test

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class MyPlugin(Star):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
 
     # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
+    @filter.event_message_type(filter.EventMessageType.PRIVATE_MESSAGE)
     async def helloworld(self, event: AstrMessageEvent):
         """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
         user_name = event.get_sender_name()
@@ -43,10 +43,10 @@ class MyPlugin(Star):
         elif hasattr(obj, "__dict__"):
             self.traverse_and_log_text(obj.__dict__, visited)
 
-    @filter.event_message_type(filter.EventMessageType.PRIVATE_MESSAGE)
-    async def on_all_message(self, event: AstrMessageEvent):
-        self.traverse_and_log_text(event)
-        yield event.plain_result("收到了一条消息。")
+    # @filter.event_message_type(filter.EventMessageType.PRIVATE_MESSAGE)
+    # async def on_all_message(self, event: AstrMessageEvent):
+    #     self.traverse_and_log_text(event)
+    #     yield event.plain_result("收到了一条消息。")
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
